d812ec8eff72533e91d447303f596702eb5bc6c6
Bendahma amine <mkaddourbendahma@gmail.com>
2020-08-31 20:22:55 +0100

    Initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -37385,15 +37385,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/site/style.scss":
+/*!****************************************!*\
+  !*** ./resources/sass/site/style.scss ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**********************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/site/style.scss ***!
+  \**********************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Projects\LaraPrint\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Projects\LaraPrint\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Projects\LaraShop\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\Projects\LaraShop\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\Projects\LaraShop\resources\sass\site\style.scss */"./resources/sass/site/style.scss");
 
 
 /***/ })
