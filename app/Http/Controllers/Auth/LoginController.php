c96599290de6d851ee8f2415f6dde20d044ad150
Bendahma amine <mkaddourbendahma@gmail.com>
2020-09-01 08:05:17 +0100

    adding migrations model controllers seeders

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,6 +28,12 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+
+
+    public function username(){
+        return 'username';
+    }
+
     /**
      * Create a new controller instance.
      *
---
+++ app/Manager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Manager extends Model
+{
+    protected $guarded = [];
+
+}
